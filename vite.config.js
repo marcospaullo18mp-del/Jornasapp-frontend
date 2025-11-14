213517a314ad27eece305928dea451bf5697989e
marcospaullo18mp-del <marcospaullo18.mp@gmail.com>
2025-11-14 01:13:35 -0300

    fix: remove duplicidades e corrige vite.config.js para deploy

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,3 @@
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    open: true
-  }
-})
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: './',
-  plugins: [react()],
-  server: {
-    port: 5173,
-    open: true
-  }
-})
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
